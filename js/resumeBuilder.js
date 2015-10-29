422aec03b2ad0d90670bdd858f8dd7ab8d5f6d81
Cory Kane <ckane30691@gmail.com>
2015-10-28 20:25:54 -0700

    Add Name & Title to Resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $('#main').append("Cory Kane");
+ //$('#main').append("Cory Kane");
+ var formattedName =  HTMLheaderName.replace("%data%", "Cory Kane");
+ var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+ $('#header').append(formattedName);
+ $('#header').append(formattedRole);
