c6452b6a4d66663b88a5b6a6e5370045131d73ca
Cory Kane <ckane30691@gmail.com>
2015-10-28 20:29:53 -0700

    Change append to prepend in resumeBuilder.js file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,5 +5,6 @@ This is empty on purpose! Your code to build the resume will go here.
  var formattedName =  HTMLheaderName.replace("%data%", "Cory Kane");
  var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 
- $('#header').append(formattedName);
- $('#header').append(formattedRole);
+$('#header').prepend(formattedRole);
+ $('#header').prepend(formattedName);
+ 
